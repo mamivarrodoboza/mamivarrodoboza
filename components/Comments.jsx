6e1379198c8ffe50a2e64c4aeb829ba01a98834b
SzathRobi <szathrobi98@gmail.com>
2022-06-20 17:47:25 +0200

    modify comments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable operator-linebreak */
 import moment from 'moment';
 import parse from 'html-react-parser';
 
@@ -7,17 +8,19 @@ const Comments = ({ relatedComments = [] }) => (
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
         {relatedComments.length} Hozzászólás
       </h3>
-      {relatedComments.reverse().map((comment, index) => (
-        <div key={index} className="border-b border-gray-100 mb-4 pb-4">
-          <p className="mb-4">
-            <span className="font-semibold">{comment.name}</span> on{' '}
-            {moment(comment.createdAt).format('YYYY MM DD')}
-          </p>
-          <p className="whitespace-pre-line text-gray-600 w-full">
-            {parse(comment.comment)}
-          </p>
-        </div>
-      ))}
+      {!relatedComments ||
+        (relatedComments.length !== 0 &&
+          relatedComments.reverse().map((comment, index) => (
+            <div key={index} className="border-b border-gray-100 mb-4 pb-4">
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span> on{' '}
+                {moment(comment.createdAt).format('YYYY MM DD')}
+              </p>
+              <p className="whitespace-pre-line text-gray-600 w-full">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          )))}
     </div>
   </>
 );
