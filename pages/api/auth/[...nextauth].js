d1646a1d957444cdd273f69e2c9d3b2dd49954d9
SzathRobi <szathrobi98@gmail.com>
2022-06-23 19:04:43 +0200

    remove google login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,10 +9,7 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+
     // ...add more providers here
   ],
   pages: {
